8a756b7e5e06c3cf6ccbab084f812271b1405072
janghyoin91 <janghyoin91@naver.com>
2019-09-22 23:31:49 +0900

    add App.jsx, index.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import InputForm from './component/InputForm';
+import { Div } from './styled';
+
+const App = () => {
+	return (
+		<Div>
+			<InputForm />
+		</Div>
+	);
+};
+
+export default App;
